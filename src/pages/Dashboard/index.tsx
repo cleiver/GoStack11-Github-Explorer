1954cff374b66bc4047428e55c2d14b1cf30e532
cleiver <cleiver@gmail.com>
2020-04-20 19:46:12 -0300

    Connecting to the Github API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,54 +1,61 @@
-import React from 'react'
+import React, { useState, FormEvent } from 'react'
 import { FiChevronRight } from 'react-icons/fi'
+import api from '../../services/api'
 
 import logo from '../../assets/logo.svg'
 
 import { Title, Form, Repositories } from './styles'
 
+interface RepositoryDTO {
+  full_name: string
+  owner: {
+    login: string
+    avatar_url: string
+  }
+  description: string
+}
+
 const Dashboard: React.FC = () => {
+  const [repositories, setRepositories] = useState<RepositoryDTO[]>([])
+
+  const [repositoryQuery, setRepositoryQuery] = useState('')
+
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault()
+
+    const response = await api.get<RepositoryDTO>(`repos/${repositoryQuery}`)
+
+    setRepositories([...repositories, response.data])
+    setRepositoryQuery('')
+  }
+
   return (
     <>
       <img src={logo} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          placeholder="Digite o nome do repositório"
+          value={repositoryQuery}
+          onChange={e => setRepositoryQuery(e.target.value)}
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="/">
-          <img src="https://avatars1.githubusercontent.com/u/31499?s=460&u=36024d382a6c412cb48c4514a5e63c95e80446f4&v=4" alt="Cleiver" />
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="/">
+            <img src={repository.owner.avatar_url} alt={repository.owner.login} />
 
-          <div>
-            <strong>GoStack Bootcamp 11</strong>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-          </div>
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="/">
-          <img src="https://avatars1.githubusercontent.com/u/31499?s=460&u=36024d382a6c412cb48c4514a5e63c95e80446f4&v=4" alt="Cleiver" />
-
-          <div>
-            <strong>GoStack Bootcamp 11</strong>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="/">
-          <img src="https://avatars1.githubusercontent.com/u/31499?s=460&u=36024d382a6c412cb48c4514a5e63c95e80446f4&v=4" alt="Cleiver" />
-
-          <div>
-            <strong>GoStack Bootcamp 11</strong>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   )
